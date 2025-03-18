3c88cdc7e44a538a00307b302c69c49e8476c773
Robin <lotosv2010@163.com>
2025-03-18 14:36:58 +0800

    feat(cache): 优化缓存

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
-const http = require('http')
-const fs = require('fs').promises
-const { createReadStream, createWriteStream, readFileSync, existsSync } = require('fs')
-const path = require('path')
-const crypto = require('crypto')
-const zlib = require('zlib')
+const http = require("http");
+const fs = require("fs").promises;
+const {
+  createReadStream,
+  createWriteStream,
+  readFileSync,
+  existsSync,
+} = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+const zlib = require("zlib");
 // 引入第三方模块
-const ejs = require('ejs')
-const debug = require('debug')('server')
-const mime = require('mime')
-const chalk = require('chalk')
-const internalIp = require('internal-ip')
-const dayjs = require('dayjs')
+const ejs = require("ejs");
+const debug = require("debug")("server");
+const mime = require("mime");
+const chalk = require("chalk");
+const internalIp = require("internal-ip");
+const dayjs = require("dayjs");
 // 引入自定义的工具函数
-const { getPermissionString, byteToSize } = require('./utils')
+const { getPermissionString, byteToSize } = require("./utils");
 
 /**
  * Server类用于创建和管理HTTP服务器
@@ -23,9 +28,9 @@ class Server {
    * @param {Object} config - 服务器配置对象，包含端口(port)、目录(directory)和主机(host)
    */
   constructor(config) {
-    this.port = config.port
-    this.directory = config.directory
-    this.host = config.host
+    this.port = config.port;
+    this.directory = config.directory;
+    this.host = config.host;
   }
 
   /**
@@ -34,7 +39,7 @@ class Server {
    * @returns {URL} 解析后的URL对象
    */
   parseUrl(request) {
-    return new URL(request.url, `http://${request.headers.host}`)
+    return new URL(request.url, `http://${request.headers.host}`);
   }
 
   /**
@@ -48,13 +53,15 @@ class Server {
 
     try {
       const stat = await fs.stat(requestUrl); // 获取文件状态
-      if (stat.isFile()) { // 文件
-        await this.sendFile(request, response, requestUrl, stat)
-      } else { // 目录
-        const concatFilePath = path.join(requestUrl, 'index.html');
+      if (stat.isFile()) {
+        // 文件
+        await this.sendFile(request, response, requestUrl, stat);
+      } else {
+        // 目录
+        const concatFilePath = path.join(requestUrl, "index.html");
         try {
           const concatStat = await fs.stat(concatFilePath);
-          await this.sendFile(request, response, concatFilePath, concatStat)
+          await this.sendFile(request, response, concatFilePath, concatStat);
         } catch (error) {
           this.showList(request, response, requestUrl, stat, url.pathname);
         }
@@ -70,7 +77,7 @@ class Server {
    * @returns {Object} 解析后的查询参数键值对
    */
   async processParams(searchParams) {
-    const params = {}
+    const params = {};
     for (const [key, value] of searchParams.entries()) {
       params[key] = value;
     }
@@ -85,31 +92,31 @@ class Server {
    * @returns {Boolean} 是否处理了Mock数据请求
    */
   async processMock(request, response, url) {
-    if (!url.pathname.startsWith('/mock/')) return; // 不是mock请求
+    if (!url.pathname.startsWith("/mock/")) return; // 不是mock请求
 
-    const mockPath = path.join(this.directory, 'mock', 'index.js'); // mock文件路径
+    const mockPath = path.join(this.directory, "mock", "index.js"); // mock文件路径
 
     // query
     request.query = await this.processParams(url.searchParams);
 
     // body
     request.body = await new Promise((resolve, reject) => {
-      const chunks = []
-      request.on('data', (chunk) => chunks.push(chunk))
-      request.on('end', () => {
-        let body = Buffer.concat(chunks).toString() || '{}';
-        switch (request.headers['content-type']) {
-          case 'application/json':
+      const chunks = [];
+      request.on("data", (chunk) => chunks.push(chunk));
+      request.on("end", () => {
+        let body = Buffer.concat(chunks).toString() || "{}";
+        switch (request.headers["content-type"]) {
+          case "application/json":
             body = JSON.parse(body);
             break;
-          case 'application/x-www-form-urlencoded':
-            body = this.processParams(new URLSearchParams(body))
+          case "application/x-www-form-urlencoded":
+            body = this.processParams(new URLSearchParams(body));
             break;
           default:
             break;
         }
         resolve(body);
-      })
+      });
     });
 
     try {
@@ -118,7 +125,7 @@ class Server {
         return mockFn(url.pathname, request, response);
       }
     } catch (error) {
-      debug('Mock file not found or invalid:', error.message);
+      debug("Mock file not found or invalid:", error.message);
     }
     return false;
   }
@@ -131,12 +138,18 @@ class Server {
    */
   async processCors(request, response) {
     if (request.headers.origin) {
-      response.setHeader('Access-Control-Allow-Origin', request.headers.origin)
-      response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-      response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-      response.setHeader('Access-Control-Max-Age', 86400)
+      response.setHeader("Access-Control-Allow-Origin", request.headers.origin);
+      response.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+      );
+      response.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      response.setHeader("Access-Control-Max-Age", 86400);
       // 处理预检请求
-      if (request.method === 'OPTIONS') {
+      if (request.method === "OPTIONS") {
         // response.statusCode = 204;
         response.end();
         return true;
@@ -163,7 +176,7 @@ class Server {
     } catch (error) {
       this.sendError(request, response, error);
     }
-  }
+  };
 
   /**
    * 显示目录列表
@@ -178,23 +191,31 @@ class Server {
     try {
       const dirs = await fs.readdir(filePath); // 获取目录包含的信息
       // 获取文件信息
-      const parseFiles = await Promise.all(dirs.map(async item => {
-        const dirStat = await fs.stat(path.join(filePath, item)); // 获取文件状态
-        console.log(item, dirStat.isFile(), dirStat.size)
-        return {
-          dir: item,
-          href: path.join(pathname, item),
-          isFile: dirStat.isFile(),
-          permission: getPermissionString(dirStat.mode),
-          size: dirStat.isFile() ? byteToSize(dirStat.size) : '',
-          time: dayjs(dirStat.mtime).format('YYYY-MM-DD HH:mm:ss')
-        }
-      }));
-      const footer = `Node.js ${process.version}/ ecstatic server running @ ${this.host}:${this.port}`
-      const template = await fs.readFile(path.join(__dirname, 'template.ejs'), 'utf-8');
-      const templateStr = await ejs.render(template, { dirs: parseFiles, footer }, { async: true });
+      const parseFiles = await Promise.all(
+        dirs.map(async (item) => {
+          const dirStat = await fs.stat(path.join(filePath, item)); // 获取文件状态
+          return {
+            dir: item,
+            href: path.join(pathname, item),
+            isFile: dirStat.isFile(),
+            permission: getPermissionString(dirStat.mode),
+            size: dirStat.isFile() ? byteToSize(dirStat.size) : "",
+            time: dayjs(dirStat.mtime).format("YYYY-MM-DD HH:mm:ss"),
+          };
+        })
+      );
+      const footer = `Node.js ${process.version}/ ecstatic server running @ ${this.host}:${this.port}`;
+      const template = await fs.readFile(
+        path.join(__dirname, "template.ejs"),
+        "utf-8"
+      );
+      const templateStr = await ejs.render(
+        template,
+        { dirs: parseFiles, footer },
+        { async: true }
+      );
 
-      response.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8;' });
+      response.writeHead(200, { "Content-Type": "text/html;charset=utf-8;" });
       response.end(templateStr);
     } catch (error) {
       this.sendError(request, response, error);
@@ -208,16 +229,16 @@ class Server {
    * @returns {Object|Boolean} 压缩流对象或false
    */
   async processCompress(request, response) {
-    const encoding = request.headers['accept-encoding'];
-    if (encoding?.includes('gzip')) {
-      response.setHeader('Content-Encoding', 'gzip');
-      return zlib.createGzip() // 创建转化流
-    } else if (encoding?.includes('br')) {
-      response.setHeader('Content-Encoding', 'br')
-      return zlib.createBrotliCompress()
-    } else if (encoding?.includes('deflate')) {
-      response.setHeader('Content-Encoding', 'deflate')
-      return zlib.createDeflate()
+    const encoding = request.headers["accept-encoding"];
+    if (encoding?.includes("gzip")) {
+      response.setHeader("Content-Encoding", "gzip");
+      return zlib.createGzip(); // 创建转化流
+    } else if (encoding?.includes("br")) {
+      response.setHeader("Content-Encoding", "br");
+      return zlib.createBrotliCompress();
+    } else if (encoding?.includes("deflate")) {
+      response.setHeader("Content-Encoding", "deflate");
+      return zlib.createDeflate();
     }
     return false;
   }
@@ -231,28 +252,27 @@ class Server {
    * @returns {Boolean} 是否命中缓存
    */
   async processCache(request, response, filePath, statObj) {
-    response.setHeader('Expires', new Date(Date.now() + 10 * 1000).toGMTString())
-    response.setHeader('Cache-Control', 'max-age=10')
+    // 1. 设置强制缓存
+    response.setHeader('Expires', new Date(Date.now() + 10 * 1000).toGMTString()); // 设置过期时间
+    response.setHeader('Cache-Control', 'max-age=10'); // 缓存10秒
 
-    const fileContent = await fs.readFile(filePath);
-
-    // Last-Modified
-    const ifModifiedSince = request.headers['if-modified-since']
-    const ctime = statObj.ctime.toGMTString()
-    // Etag
-    const ifNoneMatch = request.headers['if-none-match']
-    const etag = crypto.createHash('md5').update(fileContent).digest('base64')
-
-    response.setHeader('Last-Modified', ctime)
-    response.setHeader('Etag', etag)
-
+    // 2. 设置协商缓存
+    // 2.1 Last-Modified
+    const ifModifiedSince = request.headers["if-modified-since"];
+    const ctime = statObj.ctime.toGMTString();
+    response.setHeader("Last-Modified", ctime);
     if (ifModifiedSince !== ctime) {
-      return false
+      return false;
     }
+    // 2.2 Etag
+    const ifNoneMatch = request.headers["if-none-match"];
+    // const fileContent = await fs.readFile(filePath); // 优化etag，不再使用文件内容，使用文件大小和修改时间
+    const etag = crypto.createHash("md5").update(`${statObj.size}-${statObj.ctime}`).digest("base64");
+    response.setHeader("Etag", etag);
     if (ifNoneMatch !== etag) {
-      return false
+      return false;
     }
-    return true
+    return true;
   }
 
   /**
@@ -270,9 +290,9 @@ class Server {
         const referer = new URL(referrer).host;
         const host = request.headers.host;
         if (referer !== host) {
-          response.statusCode = 403
-          response.end('Forbidden')
-          return true
+          response.statusCode = 403;
+          response.end("Forbidden");
+          return true;
         }
       }
     }
@@ -289,10 +309,10 @@ class Server {
   async sendFile(request, response, filePath, statObj) {
     try {
       // 缓存
-      let cache = await this.processCache(request, response, filePath, statObj)
+      let cache = await this.processCache(request, response, filePath, statObj);
       if (cache) {
-        response.statusCode = 304
-        return response.end()
+        response.statusCode = 304;
+        return response.end();
       }
       // 防盗链
       if (await this.processReferer(request, response, filePath)) {
@@ -301,15 +321,15 @@ class Server {
       // 压缩
       const gzip = await this.processCompress(request, response);
       const mimeType = mime.getType(filePath);
-      response.setHeader('Content-Type', `${mimeType};charset=utf-8;`);
+      response.setHeader("Content-Type", `${mimeType};charset=utf-8;`);
 
       if (gzip) {
-        createReadStream(filePath).pipe(gzip).pipe(response)
+        createReadStream(filePath).pipe(gzip).pipe(response);
       } else {
-        createReadStream(filePath).pipe(response)
+        createReadStream(filePath).pipe(response);
       }
     } catch (e) {
-      debug('Error while sending file:', error.message);
+      debug("Error while sending file:", error.message);
       this.sendError(request, response, error);
     }
   }
@@ -322,11 +342,11 @@ class Server {
    */
   sendError(request, response, error) {
     // 调用debug函数记录错误信息
-    debug('Error occurred:', error.message);
+    debug("Error occurred:", error.message);
     // 设置响应状态码为404，表示资源未找到
-    response.statusCode = 404
+    response.statusCode = 404;
     // 结束响应，发送'Not Found'消息给客户端
-    response.end('Not Found')
+    response.end("Not Found");
   }
 
   /**
@@ -336,19 +356,37 @@ class Server {
    */
   start() {
     // 创建HTTP服务器，处理请求时调用handleRequest方法
-    const server = http.createServer(this.handleRequest.bind(this))
+    const server = http.createServer(this.handleRequest.bind(this));
 
     // 服务器监听指定的主机和端口
     server.listen(this.port, this.host, async () => {
       // 打印启动信息，包括服务的目录
-      console.log(chalk.yellowBright(`Starting up http-server, serving ./${this.directory.split('\/').pop()}\r\n`))
+      console.log(
+        chalk.yellowBright(
+          `Starting up http-server, serving ./${this.directory
+            .split("/")
+            .pop()}\r\n`
+        )
+      );
       // 打印服务可用的URL
-      console.log(chalk.yellowBright(`Available on:`))
-      console.log(` http://${this.host}:${chalk.green(this.port)}`)
-      console.log(` http://${await internalIp.v4()}:${chalk.green(this.port)}`)
+      console.log(chalk.yellowBright(`Available on:`));
+      console.log(` http://${this.host}:${chalk.green(this.port)}`);
+      console.log(` http://${await internalIp.v4()}:${chalk.green(this.port)}`);
       // 提示用户如何停止服务器
-      console.log(chalk.blueBright(`Hit CTRL-C to stop the server`))
-    })
+      console.log(chalk.blueBright(`Hit CTRL-C to stop the server`));
+    });
+
+    // 监听服务器错误事件，如果端口被占用，则尝试使用下一个端口
+    server.on("error", (error) => {
+      if (error.code === "EADDRINUSE") {
+        console.log(
+          chalk.red(
+            `Port ${this.port} is already in use. Trying another port...`
+          )
+        );
+        server.listen(++this.port);
+      }
+    });
   }
 }
-module.exports = Server
+module.exports = Server;
